b3d32ac075241febb42c80313dccd2913a772bb5
Will <will.lucen4@gmail.com>
2021-02-22 21:28:27 -0300

    day one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      Hello NLW
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
