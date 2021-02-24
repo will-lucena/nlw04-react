b216e87dc605a28ba0c582d97167afc4c1898c3f
Will <will.lucen4@gmail.com>
2021-02-23 21:02:42 -0300

    index module css added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { ExperienceBar } from "../components/ExperienceBar";
+import Profile from "../components/Profile";
+
+import styles from '../styles/components/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile></Profile>
+        </div>
+      </section>
     </div>
   );
 }
