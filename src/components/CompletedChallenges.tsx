be01cb9bc5dc14c86643b63057886becc30a76f4
Will <will.lucen4@gmail.com>
2021-02-23 23:07:34 -0300

    completed challenges component added

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges(){
+  return (
+    <div className={styles.container}>
+      <span>Completed Challenges</span>
+      <span>05</span>
+    </div>
+  )
+} 
