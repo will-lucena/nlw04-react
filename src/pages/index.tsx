8ef345c0b1a0e561040c608c27be9dc1b815c52e
Will <will.lucen4@gmail.com>
2021-02-23 23:08:09 -0300

    favicon and title added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Head from "next/head";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import Profile from "../components/Profile";
 
@@ -6,10 +9,16 @@ import styles from '../styles/components/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Home - MoveIt</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
-          <Profile></Profile>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
       </section>
     </div>
