a058c5d922068be476294d398ad6f798a04e449e
Will <will.lucen4@gmail.com>
2021-02-26 20:58:13 -0300

    cache using cookies and nextJS features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import {GetServerSideProps} from 'next'
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
@@ -7,27 +8,52 @@ import Profile from "../components/Profile";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from '../styles/components/Home.module.css'
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
+interface HomeProps{
+  level: Number,
+  currentExperience: Number,
+  challengesCompleted: Number
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
+    <ChallengesProvider 
+      level={props.level} 
+      currentExperience={props.currentExperience} 
+      challengesCompleted={props.challengesCompleted}>
+        <div className={styles.container}>
 
-      <Head>
-        <title>Home - MoveIt</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <Head>
+          <title>Home - MoveIt</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
+}
+
+export const getServerSideProps:GetServerSideProps = async (context) => {
+
+  const {level, currentExperience, challengesCompleted} = context.req.cookies
+
+  return {
+    props:{
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
